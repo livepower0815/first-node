627d6878ddb366537908698553e4ea7307948bc2
KerryChen <kerry.chen@thinkpower-info.com>
2020-01-30 23:56:19 +0800

    add chat app

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,42 +1,73 @@
-const http = require('http')
 const path = require('path')
+const http = require('http')
 const express = require('express')
-const socket = require('socket.io')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
-const io = socket(server)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let message = 'default'
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
 
-io.on('connect', (socket) => {
-  console.log('New Websocket connection')
-  socket.emit('messageUpdated', 'Welcome!')
-  socket.broadcast.emit('messageUpdated', 'A new user has joined!')
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
 
-  socket.on('message', (messge, callback) => {
-    io.emit('messageUpdated', messge)
-    callback()
-  })
+        if (error) {
+            return callback(error)
+        }
 
-  socket.on('sendLocation', (coords, callback) => {
-    if (!coords.latitude || !coords.longitude) {
-      return callback('position is indefined')
-    }
-    io.emit('messageUpdated', `https://google.com/maps/@${coords.latitude},${coords.longitude},15z`)
-    callback()
-  })
+        socket.join(user.room)
 
-  socket.on('disconnect', () => {
-    io.emit('messageUpdated', 'A user has left!')
-  })
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.to(user.room).emit('message', generateMessage(user.username, message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
 })
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}`)
+    console.log(`Server is up on port ${port}!`)
 })
